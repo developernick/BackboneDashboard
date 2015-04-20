f52e5d83899eba1155ce2c562eb25435a0a2db26
nselesky <nickolas.selesky@gmail.com>
2015-04-20 16:32:18 -0500

    uuhhhh

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,31 @@
 $(document).ready(function(event) {
 
+  $(document).ready(function(event) {
+
+    var halfCardView = Backbone.View.extend({
+
+      tagName: "article",
+      className: "block-half shadow",
+      template: _.template("<h3><%= title %></h3><%= message %><span class='close'>(x) dismiss</span>"),
+      initialize: function(data) {
+        this.render(data);
+      },
+      render: function(data) {
+        this.$el.html(this.template(data));
+        //$('body').append(this.$el);
+        var row = $('.row')[1];
+        $(row).append(this.$el);
+      }
+    });
+
+    var data = { title: "Fans React to Superman vs Batman Teaser", message: "Ben Affleck is still dissappointing"}
+    var newCard = new halfCardView(data);
+
+    var dataTwo = { title: "Ben Affleck Reacts", message: "By depositing fat check in his bank account"};
+    var anotherCard = new halfCardView(dataTwo);
+
+  });
+
+
+
 });
