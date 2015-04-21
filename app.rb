8bb35f9515645450ec86746411e9aa714b392cfc
nselesky <nickolas.selesky@gmail.com>
2015-04-21 16:37:09 -0500

    completed Collection and Model

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
 require 'bundler'
 Bundler.require()
-
+# ---------------Connections---------------
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :database => 'all_the_cards'
 )
-
+# ---------------Models---------------
 require './models/card'
 
+# ---------------Helper---------------
+
+def card_parameters
+  request_body = JSON.parse(request.body.read.to_s)#Replaces content_type :json
+  { title: request_body["title"], message: request_body["message"] }
+end
 
 get '/' do
   erb :index
+end
+
+get '/variables' do
+  erb :variables
 end
 
 
@@ -26,22 +36,21 @@ get '/api/cards/:id' do
 end
 
 post '/api/cards' do
+  card = Card.create(card_parameters)
+  card.to_json
+end
+
+patch '/api/cards/:id' do
+  card = Card.find(card_parameters)
+  card.update(card_parameters)
   content_type :json
-  card = Card.create(params[:card])
   card.to_json
 end
 
 put '/api/cards/:id' do
+  card = Card.find(card_parameters)
+  card.update(card_parameters)
   content_type :json
-  card = Card.find(params[:id].to_i)
-  card.update(params[:card])
-  card.to_json
-end
-
-put '/api/cards/:id' do
-  content_type :json
-  card = Card.find(params[:id].to_i)
-  card.update(params[:card])
   card.to_json
 end
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,30 @@
 $(document).ready(function(event) {
 
-    var halfCardView = Backbone.View.extend({
+  //             -_-_-_-_-_-_-_-Model (cardModel)-_-_-_-_-_-_-_-
+  var cardModel = Backbone.Model.extend({
+    initialize: function() {
+    console.log("Hey, a card is here!");
+	 }
+  });
 
+  //          -_-_-_-_-_-_-_-Collection (cardList)-_-_-_-_-_-_-_-
+  var cardList = Backbone.Collection.extend({
+    initialize: function() {
+        console.log("Doctor Leo Marvin!!");
+    },
+    url: "/api/cards",
+    model: cardModel
+});
+  var list = new cardList();
+  list.fetch();
+  list.models.length
+  list.create({
+    title:"Quotes of the day"
+    message:"Baby steps...Baby steps through the collection"
+  });
+
+  //          -_-_-_-_-_-_-_-View (halfCardView)-_-_-_-_-_-_-_-
+    var halfCardView = Backbone.View.extend({
       tagName: "article",
       className: "block-half shadow",
       id: '<%= id %>',
@@ -18,7 +41,6 @@ $(document).ready(function(event) {
     });
 
   var thirdCardView = Backbone.View.extend({
-
     tagName: 'article',
     className: 'block-third shadow',
     id: '<%= id %>',
@@ -33,9 +55,7 @@ $(document).ready(function(event) {
     },
   });
 
-
   var quarterCardView = Backbone.View.extend({
-
     tagName: 'article',
     className: 'block-quarter shadow',
     id: '<%= id %>',
@@ -54,7 +74,6 @@ $(document).ready(function(event) {
 
   var data = { id:"one", title: "Fans React to Superman vs Batman Teaser", message: "Ben Affleck is still dissappointing"};
   var newCard = new halfCardView(data);
-
   var dataTwo = { id: "two", title: "Ben Affleck Reacts", message: "By depositing fat check in his bank account"};
   var twoCard = new halfCardView(dataTwo);
 
@@ -62,25 +81,20 @@ $(document).ready(function(event) {
 
   var dataThree = { id: "three", title: "The Third", message: "This is the Third1"};
   var threeCard = new thirdCardView(dataThree);
-
-  var dataFour = { id: "four", title: "The third", message: "This is the third2"};
+  var dataFour = { id: "four", title: "The Third", message: "This is the Third2"};
   var fourCard = new thirdCardView(dataFour);
-
-  var dataFive = { id: "five", title: "The third", message: "This is the third3"};
+  var dataFive = { id: "five", title: "The Third", message: "This is the Third3"};
   var fiveCard = new thirdCardView(dataFive);
 
-//              _-_-_-_-_-_-Quarter Cards_-_-_-_-_-_-
+//               _-_-_-_-_-_-Quarter Cards_-_-_-_-_-_-
 
-  var dataSix = { id: "six", title: "Fourth", message: "This is the fourth1"};
+  var dataSix = { id: "six", title: "Fourth", message: "This is the Fourth1"};
   var sixCard = new quarterCardView(dataSix);
-
-  var dataSeven = { id: "seven", title: "Fourth", message: "This is the fourth2"};
+  var dataSeven = { id: "seven", title: "Fourth", message: "This is the Fourth2"};
   var sevenCard = new quarterCardView(dataSeven);
-
-  var dataEight = { id: "eight", title: "Fourth", message: "This is the fourth3"};
+  var dataEight = { id: "eight", title: "Fourth", message: "This is the Fourth3"};
   var eightCard = new quarterCardView(dataEight);
-
-  var dataNine = { id: "nine", title: "Fourth", message: "This is the fourth4"};
+  var dataNine = { id: "nine", title: "Fourth", message: "This is the Fourth4"};
   var nineCard = new quarterCardView(dataNine);
 
 });
